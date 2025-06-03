a2ab6a4d703bb0ff913c448732c336295984f3e3
Ilya Grishanov <grishanov@me.com>
2025-06-03 14:09:32 +0300

    fix: MainStoryboard has been removed

--- FakeNFT/AppDelegate.swift
+++ FakeNFT/AppDelegate.swift
@@ -6,16 +6,20 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _: UIApplication,
         didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
 
     func application(
         _: UIApplication,
         configurationForConnecting connectingSceneSession: UISceneSession,
         options _: UIScene.ConnectionOptions
     ) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        let configuration = UISceneConfiguration(
+            name: "Default Configuration",
+            sessionRole: connectingSceneSession.role
+        )
+        configuration.delegateClass = SceneDelegate.self
+        return configuration
     }
 }
--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -1,25 +1,27 @@
 import UIKit
 
 final class TabBarController: UITabBarController {
+    var servicesAssembly: ServicesAssembly! {
+        didSet {
+            setupViewControllers()
+        }
+    }
 
-    var servicesAssembly: ServicesAssembly!
-
-    private let catalogTabBarItem = UITabBarItem(
-        title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
-        tag: 0
-    )
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    private func setupViewControllers() {
+        guard servicesAssembly != nil else { return }
+        
+        let catalogTabBarItem = UITabBarItem(
+            title: NSLocalizedString("Tab.catalog", comment: ""),
+            image: UIImage(systemName: "square.stack.3d.up.fill"),
+            tag: 0
+        )
+        
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
         catalogController.tabBarItem = catalogTabBarItem
-
+        
         viewControllers = [catalogController]
-
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemBackground 
     }
 }
