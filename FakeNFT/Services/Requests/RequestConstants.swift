96acb4b6f2c0030e31f27d2f4cbbab296ed34522
BolshakovDmitry <bolshakoff.dima@yandex.ru>
2025-06-08 15:11:19 +0300

    Создал ветку эпика Корзина

--- FakeNFT/Services/Requests/RequestConstants.swift
+++ FakeNFT/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-    #warning("Instert your token here")
-    static let token = ""
+    static let token = "8fe8bbb3-b61e-4eff-8973-473d07ceba1c"
 }
